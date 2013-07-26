ac899c7ac81ac1603f4389d960e69335040fece2
Cosmosfarm <support@cosmosfarm.com>
2013-07-26 12:43:12 +0900

    버그 수정

--- plugins/kboard/Content.class.php
+++ plugins/kboard/Content.class.php
@@ -16,7 +16,7 @@ class Content {
 	
 	// 스킨에서 사용 할 첨부파일 input[type=file] 이름의 prefix를 정의한다.
 	var $skin_attach_prefix = 'kboard_attach_';
-	// 스킨에서 사용 할 사용자 정의 input[type=text] 이름의 prefix를 정의한다.
+	// 스킨에서 사용 할 사용자 정의 옵션 input, textarea, select 이름의 prefix를 정의한다.
 	var $skin_option_prefix = 'kboard_option_';
 	
 	var $attach_store_path;
@@ -26,6 +26,7 @@ class Content {
 	
 	function __construct($board_id=''){
 		if($board_id) $this->setBoardID($board_id);
+		$this->row = new stdClass();
 		$this->next = $_POST['next'];
 	}
 	
@@ -37,6 +38,10 @@ class Content {
 		$this->row->{$name} = $value;
 	}
 	
+	/**
+	 * 게시판 ID를 입력받는다.
+	 * @param int $board_id
+	 */
 	function setBoardID($board_id){
 		$this->board_id = $board_id;
 		
@@ -46,6 +51,11 @@ class Content {
 		$this->thumbnail_store_path = str_replace(KBOARD_WORDPRESS_ROOT, '', $upload_dir['basedir']) . "/kboard_thumbnails/$board_id/" . date("Ym", current_time('timestamp')) . '/';
 	}
 	
+	/**
+	 * 게시글 고유번호를 입력받아 정보를 초기화한다.
+	 * @param int $uid
+	 * @return Content
+	 */
 	function initWithUID($uid){
 		if($uid){
 			$this->row = mysql_fetch_object(kboard_query("SELECT * FROM kboard_board_content WHERE uid=$uid LIMIT 1"));
@@ -55,6 +65,11 @@ class Content {
 		return $this;
 	}
 	
+	/**
+	 * 게시글 정보를 입력받는다.
+	 * @param object $row
+	 * @return Content
+	 */
 	function initWithRow($row){
 		if($row){
 			$this->row = $row;
@@ -64,6 +79,9 @@ class Content {
 		return $this;
 	}
 	
+	/**
+	 * 게시글을 등록/수정한다.
+	 */
 	function execute(){
 		$this->member_uid = $_POST['member_uid'];
 		$this->member_display = $_POST['member_display'];
@@ -99,6 +117,10 @@ class Content {
 		if($this->next) die("<script>location.href='$this->next';</script>");
 	}
 	
+	/**
+	 * 게시글을 등록한다.
+	 * @return int
+	 */
 	private function _insertContent(){
 		global $user_ID;
 		$userdata = get_userdata($user_ID);
@@ -142,6 +164,9 @@ class Content {
 		return mysql_insert_id();
 	}
 	
+	/**
+	 * 게시글 정보를 수정한다.
+	 */
 	private function _updateContent(){
 		if($this->uid){
 			$data['board_id'] = $this->board_id;
@@ -160,7 +185,6 @@ class Content {
 				$value = addslashes($value);
 				$update[] = "`$key`='$value'";
 			}
-			
 			kboard_query("UPDATE kboard_board_content SET ".implode(',', $update)." WHERE uid=$this->uid");
 		}
 	}
@@ -175,8 +199,13 @@ class Content {
 		}
 	}
 	
+	/**
+	 * 게시글 옵션 정보를 초기화 한다.
+	 * @return string
+	 */
 	function initOptions(){
 		if(!$this->uid) return '';
+		$option = array();
 		$result = kboard_query("SELECT * FROM kboard_board_option WHERE content_uid=$this->uid");
 		while($row = mysql_fetch_array($result)){
 			$option[$row['option_key']] = stripslashes($row['option_value']);
@@ -185,8 +214,13 @@ class Content {
 		return $option;
 	}
 	
+	/**
+	 * 게시글 첨부파일 정보를 초기화 한다.
+	 * @return array
+	 */
 	function initAttachedFiles(){
 		if(!$this->uid) return '';
+		$file = array();
 		$result = kboard_query("SELECT * FROM kboard_board_attached WHERE content_uid=$this->uid");
 		while($row = @mysql_fetch_array($result)){
 			$file[$row['file_key']] = array($row['file_path'], $row['file_name']);
@@ -195,6 +229,10 @@ class Content {
 		return $file;
 	}
 	
+	/**
+	 * 게시글의 첨부파일을 업데이트한다. (입력/수정/삭제)
+	 * @param int $uid
+	 */
 	function update_attach($uid){
 		if(!$this->attach_store_path) die('업로드 경로가 없습니다. 게시판 ID를 입력하고 초기화 해주세요.');
 		
@@ -221,6 +259,13 @@ class Content {
 		}
 	}
 	
+	/**
+	 * 첨부파일 정보를 수정한다.
+	 * @param int $uid
+	 * @param string $key
+	 * @param string $file_path
+	 * @param string $file_name
+	 */
 	private function _update_attach($uid, $key, $file_path, $file_name){
 		$key = addslashes($key);
 		$file_path = addslashes($file_path);
@@ -228,6 +273,13 @@ class Content {
 		kboard_query("UPDATE kboard_board_attached SET file_path='$file_path', file_name='$file_name' WHERE file_key LIKE '$key' AND content_uid=$uid");
 	}
 	
+	/**
+	 * 첨부파일 정보를 등록한다.
+	 * @param int $uid
+	 * @param string $key
+	 * @param string $file_path
+	 * @param string $file_name
+	 */
 	private function _insert_attach($uid, $key, $file_path, $file_name){
 		$date = date("YmdHis", current_time('timestamp'));
 		$key = addslashes($key);
@@ -236,6 +288,10 @@ class Content {
 		kboard_query("INSERT INTO kboard_board_attached (content_uid, file_key, date, file_path, file_name) VALUE ($uid, '$key', '$date', '$file_path', '$file_name')");
 	}
 	
+	/**
+	 * 게시글의 모든 첨부파일을 삭제한다.
+	 * @param int $uid
+	 */
 	private function _remove_all_attached($uid){
 		$result = kboard_query("SELECT file_path FROM kboard_board_attached WHERE content_uid=$uid");
 		while($file = mysql_fetch_row($result)){
@@ -244,6 +300,10 @@ class Content {
 		kboard_query("DELETE FROM kboard_board_attached WHERE content_uid=$uid");
 	}
 	
+	/**
+	 * 첨부파일을 삭제한다.
+	 * @param string $key
+	 */
 	public function removeAttached($key){
 		if($this->uid){
 			$key = addslashes($key);
@@ -256,6 +316,10 @@ class Content {
 		}
 	}
 	
+	/**
+	 * 게시글의 옵션을 업데이트한다. (입력/수정/삭제)
+	 * @param int $uid
+	 */
 	function update_options($uid){
 		foreach($_REQUEST AS $key => $value){
 			if(strstr($key, $this->skin_option_prefix) && trim($value)){
@@ -275,22 +339,45 @@ class Content {
 		$this->_remove_empty_option();
 	}
 	
+	/**
+	 * 옵션 값을 변경한다.
+	 * @param int $uid
+	 * @param string $key
+	 * @param string $value
+	 */
 	private function _update_option($uid, $key, $value){
 		kboard_query("UPDATE kboard_board_option SET option_value='$value' WHERE option_key LIKE '$key' AND content_uid=$uid");
 	}
 	
+	/**
+	 * 옵션을 등록한다.
+	 * @param int $uid
+	 * @param string $key
+	 * @param string $value
+	 */
 	private function _insert_option($uid, $key, $value){
 		kboard_query("INSERT INTO kboard_board_option (content_uid, option_key, option_value) VALUE ($uid, '$key', '$value')");
 	}
 	
+	/**
+	 * 빈 옵션들을 삭제한다.
+	 */
 	private function _remove_empty_option(){
-		kboard_query("DELETE FROM kboard_board_option WHERE option_value LIKE ''");
+		kboard_query("DELETE FROM kboard_board_option WHERE option_value=''");
 	}
 	
+	/**
+	 * 옵션을 삭제한다.
+	 * @param int $uid
+	 */
 	private function _remove_option($uid){
 		kboard_query("DELETE FROM kboard_board_option WHERE content_uid=$uid");
 	}
 	
+	/**
+	 * 썸네일을 등록한다.
+	 * @param int $uid
+	 */
 	function setThumbnail($uid){
 		if(!$this->thumbnail_store_path) die('업로드 경로가 없습니다. 게시판 ID를 입력하고 초기화 해주세요.');
 		
@@ -307,6 +394,9 @@ class Content {
 		}
 	}
 	
+	/**
+	 * 썸네일 파일을 삭제한다.
+	 */
 	function removeThumbnail(){
 		if($this->uid){
 			$result = kboard_query("SELECT * FROM kboard_board_content WHERE uid=$this->uid LIMIT 1");
@@ -318,6 +408,10 @@ class Content {
 		}
 	}
 	
+	/**
+	 * 게시글을 삭제한다.
+	 * @param string $next
+	 */
 	function remove($next=''){
 		if($this->uid){
 			$this->_remove_option($this->uid);
@@ -325,10 +419,7 @@ class Content {
 			$this->removeThumbnail();
 			kboard_query("DELETE FROM kboard_board_content WHERE uid=$this->uid");
 			if(defined('KBOARD_COMMNETS_VERSION')) kboard_query("DELETE FROM kboard_comments WHERE content_uid=$this->uid");
-			if($next){
-				echo "<script>location.href='$next';</script>";
-				exit;
-			}
+			if($next) die("<script>location.href='$next';</script>");
 		}
 	}
 	
