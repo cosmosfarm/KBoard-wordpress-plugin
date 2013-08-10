7f0905815f2c1fec4ee4d3aca567eb2891a6a55b
Cosmosfarm <support@cosmosfarm.com>
2013-08-11 01:50:32 +0900

    KBMail 수정

--- plugins/kboard/KBMail.class.php
+++ plugins/kboard/KBMail.class.php
@@ -21,8 +21,8 @@ class KBMail {
 
 		$headers = 'MIME-Version: 1.0' . "\r\n";
 		$headers .= 'Content-type: text/html; charset=euckr' . "\r\n";
-		$headers .= "To: " . $this->to . "\r\n";
-		$headers .= "From: " . $admin_email . "\r\n";
+		$headers .= 'To: ' . $this->to . "\r\n";
+		$headers .= 'From: ' . $admin_email . "\r\n";
 
 		$title = '[KBoard 신규등록] ' . $this->title;
 		$content = nl2br($this->content) . '<br><br><a href="'.site_url().'" onclick="window.open(this.href); return false;">'.site_url().'</a>';
