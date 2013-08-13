0f6481ebfd953cb2fc744606580d9df9cafdfc4b
Cosmosfarm <support@cosmosfarm.com>
2013-08-13 16:44:57 +0900

    경로관련 버그 수정

--- plugins/kboard/execute/download.php
+++ plugins/kboard/execute/download.php
@@ -16,14 +16,14 @@ $content = new Content();
 $content->initWithUID($uid);
 $board = new KBoard($content->board_id);
 if(!$board->isReader($content->member_uid, $content->secret)){
-	if(!$user_ID) die('<script>alert("로그인 하셔야 사용할 수 있습니다."); location.href="' . site_url('/wp-login.php') . '";</script>');
+	if(!$user_ID) die('<script>alert("로그인 하셔야 사용할 수 있습니다.");location.href="'.site_url('/wp-login.php').'";</script>');
 	else die('<script>alert("권한이 없습니다.");history.go(-1);</script>');
 }
 
 $result = kboard_query("SELECT * FROM ".KBOARD_DB_PREFIX."kboard_board_attached WHERE content_uid=$uid AND file_key LIKE '$file'");
 $file_info = mysql_fetch_array($result);
 
-$path = reset(explode(DIRECTORY_SEPARATOR . 'wp-content', dirname(__FILE__) . DIRECTORY_SEPARATOR)) . str_replace('/', DIRECTORY_SEPARATOR, $file_info['file_path']);
+$path = reset($path) . str_replace('/', DIRECTORY_SEPARATOR, $file_info['file_path']);
 $name = $file_info['file_name'];
 
 if(eregi("(MSIE 5.0|MSIE 5.1|MSIE 5.5|MSIE 6.0)", $_SERVER["HTTP_USER_AGENT"]) && !eregi("(Opera|Netscape)", $_SERVER["HTTP_USER_AGENT"])){
