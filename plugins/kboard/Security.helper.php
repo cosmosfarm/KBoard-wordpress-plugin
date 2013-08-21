e8f4f2f2015f986226557ad5f1024524795ce2b5
Cosmosfarm <support@cosmosfarm.com>
2013-08-21 13:05:11 +0900

    게시글 입력 태그 추가

--- plugins/kboard/Security.helper.php
+++ plugins/kboard/Security.helper.php
@@ -26,6 +26,9 @@ function kboard_xssfilter($data){
 	$HTMLPurifier_Config->set('URI.SafeIframeRegexp', '(.*)');
 	$HTMLPurifier_Config->set('HTML.TidyLevel', 'light');
 	$HTMLPurifier_Config->set('HTML.SafeObject', true);
+	$HTMLPurifier_Config->set('HTML.SafeEmbed', true);
+	$HTMLPurifier_Config->set('HTML.Allowed', 'object[width|height|data],param[name|value],embed[src|type|allowscriptaccess|width|height|wmode]');
+	$HTMLPurifier_Config->set('Output.FlashCompat', true);
 	$HTMLPurifier_Config->set('Core.DefinitionCache', null);
 	$HTMLPurifier = HTMLPurifier::getInstance();
 	$data = $HTMLPurifier->purify($data, $HTMLPurifier_Config);
