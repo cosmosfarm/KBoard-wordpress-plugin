1931a3ed58b39ab5418c5521761bc874ff7ab754
Cosmosfarm <support@cosmosfarm.com>
2013-08-24 19:19:49 +0900

    관리자 페이지 수정

--- plugins/kboard/pages/kboard_setting.php
+++ plugins/kboard/pages/kboard_setting.php
@@ -8,7 +8,7 @@ if(!defined('KBOARD_COMMNETS_VERSION')){
 <div class="wrap">
 	<div style="float: left; margin: 7px 8px 0 0; width: 36px; height: 34px; background: url(<?=plugins_url('kboard/images/icon-big.png')?>) left top no-repeat;"></div>
 	<h2>
-		KBoard : 게시판 정보수정
+		KBoard : 게시판 관리
 		<a href="http://www.cosmosfarm.com/products/kboard" class="add-new-h2" onclick="window.open(this.href); return false;">홈페이지</a>
 		<a href="http://www.cosmosfarm.com/threads" class="add-new-h2" onclick="window.open(this.href); return false;">질문하기</a>
 		<a href="http://www.cosmosfarm.com/support" class="add-new-h2" onclick="window.open(this.href); return false;">기능추가 및 기술지원</a>
