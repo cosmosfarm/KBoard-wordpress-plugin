bad04574c6dc77352bc65c20b3491983a982372a
Cosmosfarm <support@cosmosfarm.com>
2013-09-11 13:47:25 +0900

    Autolink.helper.php 파일 추가

---
+++ plugins/kboard/helper/Autolink.helper.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * KBoard 게시글 본문 자동링크
+ * @link www.cosmosfarm.com
+ * @copyright Copyright 2013 Cosmosfarm. All rights reserved.
+ * @license http://www.gnu.org/licenses/gpl.html
+ */
+function Kboard_autolink($text){
+	/*
+	 * Mark Goldsmith
+	 * http://css-tricks.com/snippets/php/find-urls-in-text-make-links/
+	 */
+	return preg_replace_callback('#(?i)(http|https)?(://)?(([-\w^@]+\.)+(kr|co.kr|go.kr|net|org|edu|gov|me|com+)(?:/[^,.\s]*|))#', 'Kboard_autolink_prependHTTP', $text);
+}
+function Kboard_autolink_prependHTTP($m){
+	/*
+	 * Mark Goldsmith
+	 * http://css-tricks.com/snippets/php/find-urls-in-text-make-links/
+	 */
+	$mStr = $m[1].$m[2].$m[3];
+	if(preg_match('#([a-z0-9&\-_.]+?)@([\w\-]+\.([\w\-\.]+\.)*[\w]+)#', $mStr)){
+		return "<a href=\"mailto:".$m[2].$m[3]."\" onclick=\"window.open(this.href); return false;\">".$m[1].$m[2].$m[3]."</a>";
+	}
+	else{
+		$http = (!preg_match("#(https://)#", $mStr)) ? 'http://' : 'https://';
+		return "<a href=\"".$http.$m[3]."\" onclick=\"window.open(this.href); return false;\">".$m[1].$m[2].$m[3]."</a>";
+	}
+}
+?>
