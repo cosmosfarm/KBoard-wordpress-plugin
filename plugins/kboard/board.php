51dc670209791eb40b080d03eb44e85674f35755
Cosmosfarm <support@cosmosfarm.com>
2014-01-12 17:50:01 +0900

    홈페이지 주소 수정

--- plugins/kboard/board.php
+++ plugins/kboard/board.php
@@ -5,7 +5,7 @@ include $path.DIRECTORY_SEPARATOR.'wp-load.php';
 /**
  * KBoard 게시판 고유주소로 요청하기
  * 입력코드(Shortcode) 없이 직접 게시판을 요청합니다.
- * @author www.cosmosfarm.com
+ * @author http://www.cosmosfarm.com/
  */
 
 $board_id = intval($_GET['board_id']);
@@ -27,7 +27,7 @@ if(!$meta->use_direct_url) exit;
 <body>
 	<!-- 
 		KBoard
-		@author www.cosmosfarm.com
+		@author http://www.cosmosfarm.com/
 	-->
 	<div id="kboard" style="float: left; width: 100%; min-height: 250px;">
 		<?php echo do_shortcode('[kboard id='.$board_id.']');?>
@@ -38,7 +38,7 @@ if(!$meta->use_direct_url) exit;
 	 * 자바스크립트 resize() 함수는 이 iframe으로 페이지를 불러올때 iframe의 height값을 자동으로 조정합니다.
 	 * 단, iframe의 id값을 'kboardframe[게시판 아이디값]'으로 설정해야 합니다.
 	 * ex: <iframe id="kboardframe1" src="kboard/board.php?board_id=1"></iframe>
-	 * @author www.cosmosfarm.com
+	 * @author http://www.cosmosfarm.com/
 	 *
 		function resize(){
 			var kboard = document.getElementById('kboard');
