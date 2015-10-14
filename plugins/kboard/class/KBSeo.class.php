d55230a9902c2e6c20544c98d1a645d398656de7
Cosmosfarm <support@cosmosfarm.com>
2015-10-14 18:38:46 +0900

    seo 최적화

--- plugins/kboard/class/KBSeo.class.php
+++ plugins/kboard/class/KBSeo.class.php
@@ -30,6 +30,7 @@ class KBSeo {
 				}
 				
 				if($is_display){
+					remove_action('wp_head', 'wp_shortlink_wp_head');
 					add_action('kboard_head', array($this, 'ogp'), 2);
 					add_action('kboard_head', array($this, 'description'), 3);
 					add_action('kboard_head', array($this, 'author'), 4);
