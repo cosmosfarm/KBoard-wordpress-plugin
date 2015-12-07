2d617f0bbd3eb67fb4575da1cd58b6f5c1dfa9ba
Cosmosfarm <support@cosmosfarm.com>
2015-12-07 21:56:19 +0900

    스킨 업데이트

--- plugins/kboard/skin/avatar/property.php
+++ plugins/kboard/skin/avatar/property.php
@@ -2,5 +2,5 @@
 $kboard['skin']['kboard-avatar']['id'] = 'kboard-avatar';
 $kboard['skin']['kboard-avatar']['name'] = 'KBoard 아바타 스킨';
 $kboard['skin']['kboard-avatar']['url'] = 'http://www.cosmosfarm.com/products/kboard';
-$kboard['skin']['kboard-avatar']['varsion'] = KBOARD_VERSION;
+$kboard['skin']['kboard-avatar']['varsion'] = '5.1';
 ?>
--- plugins/kboard/skin/thumbnail/property.php
+++ plugins/kboard/skin/thumbnail/property.php
@@ -2,5 +2,5 @@
 $kboard['skin']['kboard-thumbnail']['id'] = 'kboard-thumbnail';
 $kboard['skin']['kboard-thumbnail']['name'] = 'KBoard 썸네일 스킨';
 $kboard['skin']['kboard-thumbnail']['url'] = 'http://www.cosmosfarm.com/products/kboard';
-$kboard['skin']['kboard-thumbnail']['varsion'] = KBOARD_VERSION;
+$kboard['skin']['kboard-thumbnail']['varsion'] = '5.1';
 ?>
