8f961ed926ba68ef739bfe4c532f32ef212c8521
Cosmosfarm <support@cosmosfarm.com>
2015-12-30 16:36:51 +0900

    버그 수정

--- plugins/kboard/template/js/editor_media_button.js
+++ plugins/kboard/template/js/editor_media_button.js
@@ -23,25 +23,24 @@ function kboard_editor_open_media(){
 	var h = 500;
 	
 	if(kbaord_board_id){
-		if(jQuery(window).width() <= 600){
-			var background = jQuery('<div id="kboard_media_background"></div>').css({opacity:'0.5'});
-			var wrapper = jQuery('<div id="kboard_media_wrapper"></div>').css({width:(window.innerWidth-20), height:(window.innerHeight-20)});
-			wrapper.append(jQuery('<iframe></iframe>').attr('src', '?action=kboard_media&board_id='+kbaord_board_id+'&media_group='+kbaord_media_group+'&content_uid='+kbaord_content_uid));
-			
-			jQuery(window).resize(function(){
-				wrapper.css({width:(window.innerWidth-20), height:(window.innerHeight-20)});
-			});
-		}
-		else{
-			var background = jQuery('<div id="kboard_media_background"></div>').css({opacity:'0.5'});
-			var wrapper = jQuery('<div id="kboard_media_wrapper"></div>').css({left:'50%', top:'50%', 'margin-left':(w/2)*-1, 'margin-top':(h/2)*-1, width:w, height:h});
-			wrapper.append(jQuery('<iframe></iframe>').attr('src', '?action=kboard_media&board_id='+kbaord_board_id+'&media_group='+kbaord_media_group+'&content_uid='+kbaord_content_uid));
-		}
-		
-		jQuery('body').append(background).append(wrapper);
-		background.click(function(){
+		var wrapper = jQuery('<div id="kboard_media_wrapper"></div>');
+		var background = jQuery('<div id="kboard_media_background"></div>').css({opacity:'0.5'}).click(function(){
 			kboard_media_close();
 		});
+		
+		var init_window_size = function(){
+			if(window.innerWidth <= 900){
+				wrapper.css({left:0, top:0, margin:'10px', width:(window.innerWidth-20), height:(window.innerHeight-20)});
+			}
+			else{
+				wrapper.css({left:'50%', top:'50%', margin:0, 'margin-left':(w/2)*-1, 'margin-top':(h/2)*-1, width:w, height:h});
+			}
+		}
+		init_window_size();
+		jQuery(window).resize(init_window_size);
+		
+		wrapper.append(jQuery('<iframe></iframe>').attr('src', '?action=kboard_media&board_id='+kbaord_board_id+'&media_group='+kbaord_media_group+'&content_uid='+kbaord_content_uid));
+		jQuery('body').append(background).append(wrapper);
 		
 		if(!jQuery('input[name=media_group]').filter(function(){return this.value==kbaord_media_group}).length){
 			jQuery('[name="board_id"]').parents('form').append(jQuery('<input type="hidden" name="media_group">').val(kbaord_media_group));
