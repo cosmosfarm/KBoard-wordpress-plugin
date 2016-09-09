06a986f53617bddf186633c77c1fb0eaa0ba5465
Cosmosfarm <support@cosmosfarm.com>
2016-09-09 14:52:33 +0900

    파일 삭제

--- plugins/kboard/skin/thumbnail/property.php
+++
@@ -1,6 +0,0 @@
-<?php
-$kboard['skin']['kboard-thumbnail']['id'] = 'kboard-thumbnail';
-$kboard['skin']['kboard-thumbnail']['name'] = 'KBoard 썸네일 스킨';
-$kboard['skin']['kboard-thumbnail']['url'] = 'http://www.cosmosfarm.com/products/kboard';
-$kboard['skin']['kboard-thumbnail']['varsion'] = '5.2';
-?>
