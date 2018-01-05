1ce0e4823dceeed9b98c979df9d47ee96eead84a
lchcosmosfarm <leechanghun@cosmosfarm.com>
2018-01-05 11:06:00 +0900

    테스트

--- plugins/kboard/index.php
+++ plugins/kboard/index.php
@@ -584,10 +584,12 @@ function kboard_latest_shortcode($args){
 			$with_notice = false;
 		}
 		
+		/*
 		$within_days = 0;
 		if(isset($args['within_days']) && $args['within_days']){
 			$within_days = intval($args['within_days']);
 		}
+		*/
 		
 		$latest = $board_builder->createLatest($with_notice);
 		
