7e3bfce1c7b46616daa9a35046eb3153521aeeed
Cosmosfarm <support@cosmosfarm.com>
2018-06-12 13:33:08 +0900

    버그 수정

--- plugins/kboard/class/KBContent.class.php
+++ plugins/kboard/class/KBContent.class.php
@@ -412,7 +412,9 @@ class KBContent {
 				$data['status'] = '';
 			}
 			
-			$data['title'] = $this->titleStripTags($data['title']);
+			if(isset($data['title'])){
+				$data['title'] = $this->titleStripTags($data['title']);
+			}
 			
 			// 불필요한 데이터 필터링
 			$data = kboard_array_filter($data, array('board_id', 'parent_uid', 'member_uid', 'member_display', 'title', 'content', 'date', 'update', 'view', 'comment', 'like', 'unlike', 'vote', 'category1', 'category2', 'secret', 'notice', 'search', 'thumbnail_file', 'thumbnail_name', 'status', 'password'));
