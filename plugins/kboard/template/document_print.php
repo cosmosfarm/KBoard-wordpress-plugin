8e31329eef3f9d60a4dd48adfc885c8715f12c98
leechanghun <leechanghun@cosmosfarm.com>
2018-07-09 18:33:16 +0900

    인쇄 레이아웃에 입력 필드 추가

--- plugins/kboard/template/document_print.php
+++ plugins/kboard/template/document_print.php
@@ -11,6 +11,7 @@
 <body>
 <h1><?php echo $content->title?></h1>
 <p>작성자:<?php echo $content->member_display?> / 날짜:<?php echo date('Y-m-d H:i:s', strtotime($content->date))?> / 조회수:<?php echo $content->view?></p>
+<?php echo $content->getDocumentOptionsHTML()?>
 <?php echo nl2br($content->content)?>
 
 <script>
