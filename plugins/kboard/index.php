c49b7709782313e04d9f6d63c4990cd80e6ae716
Cosmosfarm <support@cosmosfarm.com>
2019-01-14 17:35:22 +0900

    판매조회 관련 기능 개선

--- plugins/kboard/index.php
+++ plugins/kboard/index.php
@@ -1105,6 +1105,7 @@ function kboard_scripts(){
 		'point_payment' => __('Point payment', 'kboard'),
 		'cancel_point_payment' => __('Cancel point payment', 'kboard'),
 		'paypal' => __('PayPal', 'kboard'),
+		'point' => __('Point', 'kboard'),
 	);
 	wp_localize_script('kboard-script', 'kboard_localize_strings', $localize);
 }
