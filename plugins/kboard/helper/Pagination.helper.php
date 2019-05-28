704d170110856e37ae9a869a936907914d7fc19e
Cosmosfarm <support@cosmosfarm.com>
2019-05-28 13:50:01 +0900

    kboard_pagination 함수 개선

--- plugins/kboard/helper/Pagination.helper.php
+++ plugins/kboard/helper/Pagination.helper.php
@@ -3,11 +3,16 @@ if(!defined('ABSPATH')) exit;
 /**
  * KBoard 워드프레스 게시판 페이지 출력 함수
  * @link www.cosmosfarm.com
- * @copyright Copyright 2013 Cosmosfarm. All rights reserved.
+ * @copyright Copyright 2019 Cosmosfarm. All rights reserved.
  * @license http://www.gnu.org/licenses/gpl.html
  */
-function kboard_pagination($current_page, $total, $limit){
-	foreach($_GET as $key=>$value){
+function kboard_pagination($current_page, $total, $limit, $args=array()){
+	if(!is_array($args)){
+		$args = array();
+	}
+	$args = array_merge($_GET, $args);
+	
+	foreach($args as $key=>$value){
 		if(is_array($value)){
 			$query_strings[] = http_build_query(array(sanitize_key($key)=>$value));
 		}
