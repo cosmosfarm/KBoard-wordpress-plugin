8d4733224d997685bac98b8ad62941cdeb93f750
Cosmosfarm <support@cosmosfarm.com>
2019-09-02 11:39:04 +0900

    이메일 템플릿 최적화

--- plugins/kboard/class/KBMail.class.php
+++ plugins/kboard/class/KBMail.class.php
@@ -35,24 +35,6 @@ class KBMail {
 		$message = kboard_content_paragraph_breaks($this->content);
 		$message = str_replace($kboard_attched_dir, site_url($kboard_attched_dir), $message);
 		
-		if($this->url){
-			$message .= '<table border="0" cellpadding="0" cellspacing="0" class="btn btn-primary">
-			<tbody>
-			<tr>
-			<td align="center">
-			<table border="0" cellpadding="0" cellspacing="0">
-			<tbody>
-			<tr>
-			<td><a href="' . esc_url($this->url) . '" target="_blank">' . ($this->url_name ? $this->url_name : $this->url) . '</a></td>
-			</tr>
-			</tbody>
-			</table>
-			</td>
-			</tr>
-			</tbody>
-			</table>';
-		}
-		
 		$result = wp_mail($this->to, $this->title, $message, $this->headers, $this->attachments);
 		
 		remove_filter('wp_mail', array($this, 'message_template'));
@@ -68,6 +50,14 @@ class KBMail {
 	public function message_template($args){
 		$subject = $args['subject'];
 		$message = $args['message'];
+		$message = str_replace('<p>', "<p style=\"font-family: 'Apple SD Gothic Neo','Malgun Gothic',arial,sans-serif; font-size: 14px; font-weight: normal; margin: 0; margin-bottom: 15px;\">", $message);
+		
+		$call_to_actions = array();
+		if($this->url){
+			$call_to_actions = array(
+				($this->url_name ? $this->url_name : $this->url) => esc_url($this->url)
+			);
+		}
 		
 		ob_start();
 		include_once KBOARD_DIR_PATH . '/assets/email/template.php';
