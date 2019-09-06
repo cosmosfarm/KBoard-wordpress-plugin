7a84e1e5c877fd01c5118d5e8cc8f91753c4da9e
Cosmosfarm <support@cosmosfarm.com>
2019-09-06 11:41:56 +0900

    워드프레스 관리자 페이지 레이아웃 최적화

--- plugins/kboard-comments/pages/comments_list.php
+++ plugins/kboard-comments/pages/comments_list.php
@@ -1,12 +1,13 @@
 <div class="wrap">
 	<div class="kboard-header-logo"></div>
-	<h1>
-		<?php echo KBOARD_COMMENTS_PAGE_TITLE?>
-		<a href="https://www.cosmosfarm.com" class="page-title-action" onclick="window.open(this.href);return false;"><?php echo __('Home', 'kboard-comments')?></a>
-		<a href="https://www.cosmosfarm.com/threads" class="page-title-action" onclick="window.open(this.href);return false;"><?php echo __('Community', 'kboard-comments')?></a>
-		<a href="https://www.cosmosfarm.com/support" class="page-title-action" onclick="window.open(this.href);return false;"><?php echo __('Support', 'kboard-comments')?></a>
-		<a href="https://blog.cosmosfarm.com" class="page-title-action" onclick="window.open(this.href);return false;"><?php echo __('Blog', 'kboard-comments')?></a>
-	</h1>
+	<h1 class="wp-heading-inline"><?php echo KBOARD_COMMENTS_PAGE_TITLE?></h1>
+	<a href="https://www.cosmosfarm.com" class="page-title-action" onclick="window.open(this.href);return false;"><?php echo __('Home', 'kboard-comments')?></a>
+	<a href="https://www.cosmosfarm.com/threads" class="page-title-action" onclick="window.open(this.href);return false;"><?php echo __('Community', 'kboard-comments')?></a>
+	<a href="https://www.cosmosfarm.com/support" class="page-title-action" onclick="window.open(this.href);return false;"><?php echo __('Support', 'kboard-comments')?></a>
+	<a href="https://blog.cosmosfarm.com" class="page-title-action" onclick="window.open(this.href);return false;"><?php echo __('Blog', 'kboard-comments')?></a>
+	
+	<hr class="wp-header-end">
+	
 	<form method="get">
 		<input type="hidden" name="page" value="kboard_comments_list">
 		<input type="hidden" name="filter_board_id" value="<?php echo $table->filter_board_id?>">
