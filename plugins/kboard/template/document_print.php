f7fba0f519ec22f7b3221334dda311b16017c39e
leechanghun <leechanghun@cosmosfarm.com>
2020-07-22 18:12:39 +0900

    버그 수정

--- plugins/kboard/template/document_print.php
+++ plugins/kboard/template/document_print.php
@@ -12,7 +12,7 @@
 <h1><?php echo esc_html(wp_strip_all_tags($content->title))?></h1>
 <p>작성자:<?php echo esc_html($content->member_display)?> / 날짜:<?php echo date('Y-m-d H:i:s', strtotime($content->date))?> / 조회수:<?php echo number_format($content->view)?></p>
 <?php echo $content->getDocumentOptionsHTML()?>
-<?php echo nl2br($content->content)?>
+<?php echo kboard_content_paragraph_breaks($content->content)?>
 
 <script>
 window.onload = function(){
