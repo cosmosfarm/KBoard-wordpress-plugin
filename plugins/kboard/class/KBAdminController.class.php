bf46940f87e63da7b440ac81e308c7697a074791
Cosmosfarm <support@cosmosfarm.com>
2021-08-26 23:22:49 +0900

    parse_str 함수가 max_input_vars 설정에 제한되는 문제 수정

--- plugins/kboard/class/KBAdminController.class.php
+++ plugins/kboard/class/KBAdminController.class.php
@@ -595,9 +595,47 @@ class KBAdminController {
 		
 		$board_id = isset($_POST['board_id'])?$_POST['board_id']:'';
 		
+		/**
+		 * https://github.com/shukebeta/parse-str-unlimited
+		 * @param string $string
+		 * @param array $result
+		 * @return boolean
+		 */
+		function parse_str_unlimited($string, &$result) {
+			if($string === '') return false;
+			$result = array();
+			// find the pairs "name=value"
+			$pairs = explode('&', $string);
+			$params = array();
+			foreach ($pairs as $pair) {
+				// use the original parse_str() on each element
+				parse_str($pair, $params);
+				$k = key($params);
+				if(!isset($result[$k])) {
+					$result += $params;
+				} else {
+					$result[$k] = array_merge_recursive_distinct($result[$k], $params[$k]);
+				}
+			}
+			return true;
+		}
+		
+		// better recursive array merge function listed on the array_merge_recursive PHP page in the comments
+		function array_merge_recursive_distinct(array $array1, array $array2) {
+			$merged = $array1;
+			foreach ($array2 as $key => &$value) {
+				if (is_array($value) && isset($merged[$key]) && is_array($merged[$key])) {
+					$merged[$key] = array_merge_recursive_distinct($merged[$key], $value);
+				} else {
+					$merged[$key] = $value;
+				}
+			}
+			return $merged;
+		}
+		
 		$tree_category = array();
 		if(isset($_POST['tree_category'])){
-			parse_str($_POST['tree_category'], $tree_category);
+			parse_str_unlimited($_POST['tree_category'], $tree_category);
 		}
 		
 		$board = new KBoard($board_id);
