533109605f8912975ff533d33205b542ecd7c14b
Cosmosfarm Support <support@cosmosfarm.com>
2025-12-28 17:38:35 +0900

    스킨 비밀번호 확인 페이지 개선

--- plugins/kboard/skin/contact-form/confirm.php
+++ plugins/kboard/skin/contact-form/confirm.php
@@ -1,9 +1,9 @@
 <div id="kboard-contact-form-editor" class="confirm">
 	<form method="post" action="<?php echo esc_url($url->getConfirmExecute($content->uid))?>">
 		<div class="kboard-attr-row kboard-confirm-row">
-			<label class="attr-name"><?php echo __('Password', 'kboard')?></label>
+			<label class="attr-name" for="kboard-confirm-password-input"><?php echo __('Password', 'kboard')?></label>
 			<div class="attr-value">
-				<input type="password" name="password" placeholder="<?php echo __('Password', 'kboard')?>..." autofocus required>
+				<input id="kboard-confirm-password-input" type="password" name="password" placeholder="<?php echo __('Password', 'kboard')?>..." autofocus required>
 				<?php if($board->isConfirmFailed()):?>
 					<div class="description"><?php echo __('※ Your password is incorrect.', 'kboard')?></div>
 				<?php endif?>
